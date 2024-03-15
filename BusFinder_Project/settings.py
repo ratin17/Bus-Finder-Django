f39ae9e8343dfac90f657d5d283f1f33ef6249ab
Mehrab Rabbi Ratin <ratin.cse@gmail.com>
2024-03-15 12:56:49 +0600

    env added

--- BusFinder_Project/settings.py
+++ BusFinder_Project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env=environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-60k4*fxc^vdv&2xw3hw#^us$nty^r8)xdq_m*_4@htg_$mw7nz'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
